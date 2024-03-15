3982fb831759597cd6ee75a3637f02ac2c9a8d07
chhak0503 <chhak0503@gmail.com>
2024-03-15 11:49:57 +0900

    add

--- ch10/src/main/java/kr/co/ch10/security/SecurityConfig.java
+++ ch10/src/main/java/kr/co/ch10/security/SecurityConfig.java
@@ -2,6 +2,8 @@ package kr.co.ch10.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
@@ -36,6 +38,11 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
 }
---
+++ ch10/src/main/java/kr/co/ch10/service/UserService.java
@@ -0,0 +1,19 @@
+package kr.co.ch10.service;
+
+import kr.co.ch10.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public void selectUser(){
+
+    }
+
+}
