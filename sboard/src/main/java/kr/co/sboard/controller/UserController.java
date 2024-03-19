523fe34f8c5c086811154565da5ddf0065dc63da
chhak0503 <chhak0503@gmail.com>
2024-03-19 10:05:33 +0900

    이메일 인증 추가

--- sboard/src/main/java/kr/co/sboard/controller/UserController.java
+++ sboard/src/main/java/kr/co/sboard/controller/UserController.java
@@ -1,6 +1,7 @@
 package kr.co.sboard.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import kr.co.sboard.dto.TermsDTO;
 import kr.co.sboard.dto.UserDTO;
 import kr.co.sboard.service.UserService;
@@ -57,19 +58,52 @@ public class UserController {
         return "redirect:/user/register?success=200";
     }
 
+
+
     @ResponseBody
     @GetMapping("/user/{type}/{value}")
-    public ResponseEntity<?> checkUser(@PathVariable("type")  String type,
+    public ResponseEntity<?> checkUser(HttpSession session,
+                                       @PathVariable("type")  String type,
                                        @PathVariable("value") String value){
 
         int count = userService.selectCountUser(type, value);
         log.info("count : " + count);
+
+        // 이메일 인증이면
+        if(type.equals("email")){
+            log.info("email : " + value);
+            userService.sendEmailCode(session, value);
+        }
 
         // Json 생성
         Map<String, Object> resultMap = new HashMap<>();
         resultMap.put("result", count);
 
         return ResponseEntity.ok().body(resultMap);
+    }
+
+    // 이메일 인증 코드 검사
+    @ResponseBody
+    @GetMapping("/email/{code}")
+    public ResponseEntity<?> checkEmail(HttpSession session, @PathVariable("code")  String code){
+
+        String sessionCode = (String) session.getAttribute("code");
+
+        if(sessionCode.equals(code)){
+            // Json 생성
+            Map<String, Object> resultMap = new HashMap<>();
+            resultMap.put("result", true);
+
+            return ResponseEntity.ok().body(resultMap);
+        }else{
+            // Json 생성
+            Map<String, Object> resultMap = new HashMap<>();
+            resultMap.put("result", false);
+
+            return ResponseEntity.ok().body(resultMap);
+        }
+
+
     }
 
 }
