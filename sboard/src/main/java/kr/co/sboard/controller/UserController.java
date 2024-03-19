51b2c4bbfdcfa90044ecb76212e6580e62973832
chhak0503 <chhak0503@gmail.com>
2024-03-19 12:31:13 +0900

    로그인 구현

--- sboard/src/main/java/kr/co/sboard/controller/UserController.java
+++ sboard/src/main/java/kr/co/sboard/controller/UserController.java
@@ -10,10 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -27,7 +24,8 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/user/login")
-    public String login(){
+    public String login(@ModelAttribute("success") String success){
+        // 매개변수 success에 @ModelAttribute 선언으로 View 참조할 수 있음
         return "/user/login";
     }
 
@@ -55,7 +53,7 @@ public class UserController {
 
         userService.insertUser(userDTO);
 
-        return "redirect:/user/register?success=200";
+        return "redirect:/user/login?success=200";
     }
 
     @ResponseBody
