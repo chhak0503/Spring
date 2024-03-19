70f4d25082bbe2b949ba29ecece3d7ef5f4e9569
chhak0503 <chhak0503@gmail.com>
2024-03-19 12:46:29 +0900

    로그인 구현

--- sboard/src/main/java/kr/co/sboard/controller/MainController.java
+++ sboard/src/main/java/kr/co/sboard/controller/MainController.java
@@ -4,6 +4,7 @@ import kr.co.sboard.entity.User;
 import kr.co.sboard.security.MyUserDetails;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -17,6 +18,10 @@ import java.security.Principal;
 @Controller
 public class MainController {
 
+
+    // 빌드 정보 객체를 주입 받기 위해 build.gradle 파일 맨 밑에 buildInfo() 실행 해야됨
+    private final BuildProperties buildProperties;
+
     /*
         - SecurityContextHolder에 저장된 Authentication 사용자 인증객체에서 Principal 즉 MyUserDetails(User 엔티티)를 구해 View에 email 출력
         - 물론 Template View에서 직접 Thymeleaf sec속성을 이용해 Security 인증객체를 출력할 수 있지만 여기서는 직접 Security Authentication 객체에 접근
@@ -29,7 +34,15 @@ public class MainController {
         User user = userDetails.getUser();
         log.info("user : " + user);
 
-        // View에서 userEmail 출력
+
+        // 상단 BuildProperties 주입
+        String appName = buildProperties.getName();
+        String appVersion = buildProperties.getVersion();
+        log.info("appVersion : " + appVersion);
+
+        // View에 출력할 데이터 모델 참조
+        model.addAttribute("appName", appName);
+        model.addAttribute("appVersion", appVersion);
         model.addAttribute("userEmail", user.getEmail());
 
         return "/index";
