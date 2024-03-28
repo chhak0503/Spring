bda0eefbdcf9cc785ed0fa524a5888f2f8b30c0a
chhak0503 <chhak0503@gmail.com>
2024-03-28 10:09:18 +0900

    oauth 실습

---
+++ oauthTest/src/main/java/kr/co/oauth/entity/User.java
@@ -0,0 +1,30 @@
+package kr.co.oauth.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    private String uid;
+    private String pass;
+    private String name;
+    private int age;
+    private String hp;
+    private String role;
+
+    @CreationTimestamp
+    private LocalDateTime regDate;
+}
