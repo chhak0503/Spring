f01c21aa87a061156762f8dfb77164ad08c4cfa7
chhak0503 <chhak0503@gmail.com>
2025-09-17 12:12:22 +0900

    add ch05

--- ch05/src/main/java/kr/co/ch05/controller/User1Controller.java
+++ ch05/src/main/java/kr/co/ch05/controller/User1Controller.java
@@ -5,9 +5,12 @@ import kr.co.ch05.service.User1Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
@@ -23,21 +26,35 @@ public class User1Controller {
     @PostMapping("/user1/register")
     public String register(User1DTO user1DTO){
         System.out.println(user1DTO);
-
         service.regist(user1DTO);
 
         return "redirect:/user1/list";
     }
 
-
     @GetMapping("/user1/list")
-    public String list(){
+    public String list(Model model){
+        List<User1DTO> dtoList = service.findAll();
+        model.addAttribute("dtoList",dtoList);
         return "/user1/list";
     }
 
     @GetMapping("/user1/modify")
-    public String modify(){
+    public String modify(Model model, @RequestParam("uid") String uid){
+        User1DTO user1DTO = service.findById(uid);
+        model.addAttribute(user1DTO);
         return "/user1/modify";
+    }
+
+    @PostMapping("/user1/modify")
+    public String modify(User1DTO user1DTO){
+        service.modify(user1DTO);
+        return "redirect:/user1/list";
+    }
+
+    @GetMapping("/user1/delete")
+    public String delete(String uid){
+        service.remove(uid);
+        return "redirect:/user1/list";
     }
 
 }
