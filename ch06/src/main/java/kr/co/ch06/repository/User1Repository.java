763afee1a1cf550f8b69b1e908c7a290e1098b90
chhak0503 <chhak0503@gmail.com>
2025-09-22 10:37:30 +0900

    add ch06

--- ch06/src/main/java/kr/co/ch06/repository/User1Repository.java
+++ ch06/src/main/java/kr/co/ch06/repository/User1Repository.java
@@ -1,11 +1,12 @@
 package kr.co.ch06.repository;
 
 import kr.co.ch06.dto.User1DTO;
+import kr.co.ch06.entity.User1;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface User1Repository extends JpaRepository<User1DTO, Integer> {
+public interface User1Repository extends JpaRepository<User1, String> { // <엔티티, 해당 엔티티의 @ID 속성 자료형>
 
 
 
