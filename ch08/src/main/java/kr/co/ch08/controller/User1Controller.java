215abbea1c109b0370908e789b883ad6a03020bb
chhak0503 <chhak0503@gmail.com>
2025-09-24 10:17:10 +0900

    spring rest api

--- ch08/src/main/java/kr/co/ch08/controller/User1Controller.java
+++ ch08/src/main/java/kr/co/ch08/controller/User1Controller.java
@@ -17,7 +17,7 @@ public class User1Controller {
 
     private final User1Service user1Service;
 
-    @ResponseBody
+    @ResponseBody // 컨트롤러의 요청 메서드의 반환값을 View로 출력하지 않고 응답객체 Response body로 출력, JSON 출력
     @GetMapping("/user1")
     public List<User1DTO> list(){
         List<User1DTO> dtoList = user1Service.getUserAll();
@@ -27,8 +27,15 @@ public class User1Controller {
     @GetMapping("/user1/{userid}")
     public void user1(@PathVariable("userid") String userid){}
 
+    @ResponseBody
     @PostMapping("/user1")
-    public void register(User1DTO user1DTO){}
+    public String register(User1DTO user1DTO){
+
+        log.info("user1DTO={}", user1DTO);
+        user1Service.save(user1DTO);
+
+        return "success";
+    }
 
     @PutMapping("/user1")
     public void modify(User1DTO user1DTO){}
