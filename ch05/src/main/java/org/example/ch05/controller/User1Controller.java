ac4403b5285de3580e31cf166e792f95b09b772a
chhak0503 <chhak0503@gmail.com>
2026-06-17 11:04:48 +0900

    add ch05

--- ch05/src/main/java/org/example/ch05/controller/User1Controller.java
+++ ch05/src/main/java/org/example/ch05/controller/User1Controller.java
@@ -6,9 +6,11 @@ import org.example.ch05.dto.User1DTO;
 import org.example.ch05.service.User1Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
@@ -18,7 +20,14 @@ public class User1Controller {
     private final User1Service service;
 
     @GetMapping("/user1/list")
-    public String list(){
+    public String list(Model model){
+
+        // 조회 서비스 요청
+        List<User1DTO> dtoList = service.getAll();
+
+        // 모델참조: 뷰(html)에서 컨트롤러 데이터 참조
+        model.addAttribute("dtoList", dtoList);
+
         return "/user1/list";
     }
 
@@ -37,8 +46,6 @@ public class User1Controller {
 
         return "redirect:/user1/list";
     }
-
-
 
     @GetMapping("/user1/modify")
     public String modify(){
