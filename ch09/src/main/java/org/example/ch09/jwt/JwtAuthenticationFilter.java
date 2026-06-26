ac2763c87ca4939042c1fed5caeedef299a1ac93
chhak0503 <chhak0503@gmail.com>
2026-06-26 10:06:17 +0900

    add ch09

--- ch09/src/main/java/org/example/ch09/jwt/JwtAuthenticationFilter.java
+++ ch09/src/main/java/org/example/ch09/jwt/JwtAuthenticationFilter.java
@@ -8,6 +8,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -20,17 +22,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtProvider jwtProvider;
 
-    private static final String AUTH_HEADER = "Authorization";
+    private static final String COOKIE_NAME = "AUTH-TOKEN";
     private static final String TOKEN_PREFIX = "Bearer";
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
-        // 경로 구하기
-        String uri = request.getRequestURI();
-        String path = uri.substring(uri.lastIndexOf("/"));
-
-        log.info("path : " + path);
+        log.info("JwtAuthenticationFilter...1");
 
         // 토큰 추출
         Cookie[] cookies = request.getCookies();
@@ -38,8 +35,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         if(cookies != null){
             for(Cookie cookie : cookies){
-
-                if(cookie.getName().equals("AUTH-TOKEN")){
+                if(COOKIE_NAME.equals(cookie.getName())){
                     token = cookie.getValue(); // 쿠키에서 토큰 추출
                     break;
                 }
@@ -50,7 +46,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         // 토큰 검증
         try {
             if(token != null) {
+
                 jwtProvider.validateToken(token);
+
+                // 시큐리티 인증처리
+                Authentication authentication = jwtProvider.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
             }
 
             // 다음 필터(컨트롤러) 이동
